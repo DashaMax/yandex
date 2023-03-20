ba52cb6401bde3bc5b9a7bf138c0f4b40bab9f3b
DashaMax <be9emotkot@yandex.ru>
2023-03-20 22:25:21 +0300

    add triangle_of_Maxim

--- algorithms/triangle_of_Maxim.py
+++ algorithms/triangle_of_Maxim.py
@@ -3,20 +3,6 @@ from time import time
 
 
 def get_frequency_of_triangle(a: list) -> tuple:
-    # mean_frequencies = [30.0] + [round((a[i - 1][0] + a[i][0]) / 2, 6) for i in range(1, len(a))] + [4000.0]
-    # good_frequencies = []
-    #
-    # for i in range(len(mean_frequencies)):
-    #     for j in range(1, len(a)):
-    #         if a[j][1] == 'further' and abs(a[j][0] - mean_frequencies[i]) < abs(a[j - 1][0] - mean_frequencies[i]):
-    #             break
-    #         elif a[j][1] == 'closer' and abs(a[j][0] - mean_frequencies[i]) > abs(a[j - 1][0] - mean_frequencies[i]):
-    #             break
-    #         if j == len(a) - 1:
-    #             good_frequencies.append(mean_frequencies[i])
-    #
-    # return min(good_frequencies), max(good_frequencies)
-
     min_frequency, max_frequency = 30.0, 4000.0
     previous = a[0][0]
 
